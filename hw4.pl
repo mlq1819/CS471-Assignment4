4c21d0d4abee791125844ef303d49a17c4e02632
Michael Quinn <mquinn5@binghamton.edu>
2020-02-13 14:05:34 -0500

    corrected answer to 0C

--- hw4.pl
+++ hw4.pl
@@ -127,7 +127,7 @@ isBinaryTree(tree(L,R)):-
 /* Problem 0C Answer: */
 
 computeS(Op, Arg1, Arg2, Result):-
-	Result =.. [Op,Arg1,Arg2].
+	R =.. [Op,Arg1,Arg2], Result is R.
 
 /* Problem 0C Test: */
 :- computeS(-, 19, 7, 12).
