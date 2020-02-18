b76ca920807e98dc7e661d997d1091f5be3f26c2
Michael Quinn <mquinn5@binghamton.edu>
2020-02-18 12:35:41 -0500

    wrote answer to problem 4 and removed comments

--- hw4.pl
+++ hw4.pl
@@ -251,8 +251,8 @@ sumlist([Lh|Lt],Sum):-
 
 sumlist2(List,Sum) :- sumlist2(List, 0, Sum).
 sumlist2([], Sum, Sum).
-sumlist2([Lh|Lt], PSum, Sum):-
-	PSN is Psum+Lh,
+sumlist2([Lh|Lt], PartialSum, Sum):-
+	PSN is PartialSum+Lh,
 	sumlist2(Lt, PSN, Sum).
 
 /* Problem 4 Test */
